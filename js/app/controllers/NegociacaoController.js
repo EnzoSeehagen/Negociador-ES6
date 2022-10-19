c13d0c203d5491f34d9b0c8174b35d5f39d7e840
EnzoSeehagen <enzovonseehagen@gmail.com>
2022-10-19 20:31:05 -0300

    Criação da View Mensagens e configurações no controller

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -6,11 +6,14 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
         this._listaNegociacoes = new ListaNegociacoes();
-        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes)
 
+        this._negociacoesView = new NegociacoesView($('#negociacoesView'));
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem = new Mensagem();
+        this._mensagemView = new MensagemView($('#mensagemView'));
+        this._mensagemView.update(this._mensagem);
     }
 
     adiciona(event) {
@@ -18,10 +21,13 @@ class NegociacaoController {
         event.preventDefault();
     
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes)
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociação efetuada com sucesso'
+        this._mensagemView.update(this._mensagem)
+        
         this._limpaFormulario();
     
-        console.log(this._listaNegociacoes.negociacoes);
     }   
     
     _criaNegociacao() {
